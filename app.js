405f3b4e234f2591a89ac6e550f18d7352cba162
Ricardo Garcia Ruiz <xrigaru@gmail.com>
2015-06-14 19:51:14 +0200

    Signed-off-by: Ricardo Garcia Ruiz <xrigaru@gmail.com>

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var partials = require('express-partials');
+
 var routes = require('./routes/index');
 //var users = require('./routes/users');
 
@@ -20,6 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(partials());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
