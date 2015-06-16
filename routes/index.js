ad95332fa11ff26683e7ce2e2764980f3fba82ba
Ricardo Garcia Ruiz <xrigaru@gmail.com>
2015-06-16 22:38:55 +0200

    Signed-off-by: Ricardo Garcia Ruiz <xrigaru@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,16 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
+});
+/* GET author. */
+router.get('/author', function(req, res, next) {
+  res.render('author', {authors: [{ name: 'Ricardo Garcia', urlphoto: '/images/rgr.jpg', urlvideo: '' }]});
 });
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+
 
 module.exports = router;
