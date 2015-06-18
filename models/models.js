0777dd4c39bcaab496d9f6b4a6437facae7cd151
Ricardo Garcia Ruiz <xrigaru@gmail.com>
2015-06-18 22:04:47 +0200

    Signed-off-by: Ricardo Garcia Ruiz <xrigaru@gmail.com>

--- models/models.js
+++ models/models.js
@@ -2,6 +2,8 @@ var path = require('path');
 
 // Postgres DATABASE_URL = postgres://user:password@host:port/database
 // SQLite   DATABASE_URL  sqlite://:@:/
+//var dbURL = process.env.DATABASE_URL||"sqlite://:@:/";
+
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 var DB_name = (url[6]||null);
 var user 	= (url[2]||null);
