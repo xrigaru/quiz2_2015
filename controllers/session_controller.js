60ff69823a81497fc4876f14208739822b37e10a
Ricardo Garcia Ruiz <xrigaru@gmail.com>
2015-06-23 18:35:32 +0200

    20150623:1835 expiracon session
    Signed-off-by: Ricardo Garcia Ruiz <xrigaru@gmail.com>

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -33,6 +33,8 @@ exports.create = function(req, res) {
         // Crear req.session.user y guardar campos   id  y  username
         // La sesión se define por la existencia de:    req.session.user
         req.session.user = {id:user.id, username:user.username};
+		
+		req.session.cookie.expires = new Date(Date.now() + 120000);  //expiracon de la sesion
 
         res.redirect(req.session.redir.toString());// redirección a path anterior a login
     });
